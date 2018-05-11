3994d65aa1f6e587684cabdbc44c3f8e189e5353
Daniel Schep <dschep@gmail.com>
2018-05-11 10:03:06 -0400

    :tada:

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bike to work bot
+
+A twitter bot to remind you that everyday is bike to work day!!!
+
+https://twitter.com/bikeToWorkDaily
---
+++ handler.py
@@ -0,0 +1,16 @@
+from lambda_decorators import ssm_parameter_store
+import twitter
+
+
+@ssm_parameter_store([
+    '/bike2workbot/consumer_key',
+    '/bike2workbot/consumer_secret',
+    '/bike2workbot/access_token',
+    '/bike2workbot/access_token_secret',
+])
+def tweet(event, context):
+    api = twitter.Api(consumer_key=context.parameters['/bike2workbot/consumer_key'],
+                      consumer_secret=context.parameters['/bike2workbot/consumer_secret'],
+                      access_token_key=context.parameters['/bike2workbot/access_token'],
+                      access_token_secret=context.parameters['/bike2workbot/access_token_secret'])
+    api.PostUpdate('Happy bike to work day!')
